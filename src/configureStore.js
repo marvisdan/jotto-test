d661662b8db5576b79661dfc36b88a5e75179561
Marvis Dan <marvis.dan@gmail.com>
2018-09-26 17:53:20 +0200

    add redux configuration

---
+++ src/configureStore.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+import RootReducer from './reducers/index';
+
+
+export default createStore(RootReducer);
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,13 @@
+
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced
+ * @return {boolean} - new success state
+ */
+
+const successReducer = (state, action) => {
+  return null;
+};
+
+export default successReducer;
