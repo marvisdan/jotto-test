5ae70016b1ac9cb58d50c9aa35c2b5126c22ef17
Marvis Dan <marvis.dan@gmail.com>
2018-09-28 01:09:56 +0200

    complete input component render test for success=d=false

--- src/Input.js
+++ src/Input.js
@@ -4,20 +4,40 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const {success} = this.props;
+    const contents = success ? null : (
+      <form className="form-inline">
+        <input id="word-guess"
+          data-test="input-box"
+          className="m-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+      
+    );
     return (
-      <div>
-        <button></button>
+      <div data-test="component-input"> 
+        {contents}
       </div>
+      
     );
   }
-}
+} 
 
 Input.propTypes = {
 
 };
-function mapStateToProps(state) {
+function mapStateToProps({success}) {
   return{
-
+    success
   };
 };
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -13,17 +13,27 @@ import { storeFactory } from '../test/testUtils';
 const setup = (initialState ={}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />).dive();
+  return wrapper;
 };
 
 describe('render', ()=> {
-  describe('word has not been guessed', ()=> {
+  describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: false};
+      wrapper = setup(initialState);
+    });
     test('render component without error', () => {
-
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('render input box', () => {
-      
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
     });
     test('render submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(1)
       
     });
   });
